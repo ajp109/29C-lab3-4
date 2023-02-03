0c61d91dba8991bb5aa0a749aeac06a40a346db9
ajp109 <andrew.pomfret@york.ac.uk>
2023-02-03 11:29:07 +0000

    Add project-specific files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include "mbed.h"
 
+int wordcount(char const * string) {
+    int words = 0;
+
+    return words;
+}
+
 int main() {
+
+    // This text contains 11 words.
+    char const * text = "This is a  word count test.\nHere is the second line !";
+    printf("The text is:\n%s\n", text);
+    printf("It contains %d words.\n", wordcount(text));
+
     while (true) {
         sleep();
     }
